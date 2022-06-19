af84056923e229098ad531e3d38b2acd4548f15b
Valerio Oddi <oddi.1886952@studenti.uniroma1.it>
2022-06-19 22:58:33 +0200

    Aggiunto book con scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
+
     resources :books
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  
-    
     resources :library
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
