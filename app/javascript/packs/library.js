b6364fa84d7535e771cad078bb26dafc98b28138
Valerio Oddi <oddi.1886952@studenti.uniroma1.it>
2022-06-20 15:57:41 +0200

    aggiornato libreria js

--- app/javascript/packs/library.js
+++ app/javascript/packs/library.js
@@ -1,6 +1,10 @@
-
-
+//caricamento catalogo libri da noleggio
 $("#btn1").click(function () { 
     $("#main-block").load("./books")
+});
+
+//Caricamento lista di libri presi in prestito
+$("#btn1").click(function () { 
+    $("#main-block").load("./btn_rental")
 });
 
