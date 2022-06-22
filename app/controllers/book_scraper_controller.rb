7705f52979b62e5a4185d5cf43ef471157b55dcd
Valerio Oddi <oddi.1886952@studenti.uniroma1.it>
2022-06-22 18:21:25 +0200

    aggiornato scraper mondadori e unilibro

--- app/controllers/book_scraper_controller.rb
+++ app/controllers/book_scraper_controller.rb
@@ -72,8 +72,72 @@ class BookScraperController < ApplicationController
             @book_info_arr[1] = @book_info
         end
 
+        def scraperMondadori 
+            #pagina target
+            url = 'https://www.mondadoristore.it/search/?tpr=10&g='+@isbn+'&swe=N&token_s=3QTge9UXupcqPMF2KbtuSvt8gRP9lFhtDP54Clee&search-input=active'
+            #html raw
+            unparsed_page = HTTParty.get(url)
+            #html codificato
+            parsed_page = Nokogiri::HTML(unparsed_page)
+
+            author_link = []
+            author = []
+
+            for i  in 0..parsed_page.css("a.link.nti-author").size()-1 do
+                #raccolgo gli author link
+                author_link[i] = "https://www.mondadoristore.it/"+(parsed_page.css("a.link.nti-author")[i].attribute("href").value)
+                #raccolgo gli author
+                author[i] = parsed_page.css("a.link.nti-author")[i].text
+            end
+
+            @book_info = {
+                title: parsed_page.css("h1.title").text,
+                img: "https://www.mondadoristore.it/"+(parsed_page.css(".span2.spanfirst .product-image-max-heigth.new-image-gallery").attribute("src").value),
+                author: author,
+                author_link: author_link,
+                prize: parsed_page.css("span.new-price").text,
+                url: url,
+                site: "Mondadori"
+            }
+
+            @book_info_arr[2] = @book_info
+        end
+
+        def scraperUnilibro
+            #pagina target
+            url = 'https://www.unilibro.it/libri/'+@isbn
+            #html raw
+            unparsed_page = HTTParty.get(url)
+            #html codificato
+            parsed_page = Nokogiri::HTML(unparsed_page)
+
+            author_link = []
+            author = []
+
+            for i  in 0..parsed_page.css("li > span + a").size()-1 do
+                #raccolgo gli author link
+                author_link[i] = parsed_page.css("li > span + a")[i].attribute("href").value
+                #raccolgo gli author
+                author[i] = parsed_page.css("li > span + a")[i].text
+            end
+
+            @book_info = {
+                title: parsed_page.css("h1.c-libri.droid").text,
+                img: parsed_page.css("div.cover > img").attribute("src").value,
+                author: author,
+                author_link: author_link,
+                prize: parsed_page.css("li.pVendita.droid > span").text,
+                url: url,
+                site: "Unilibro"
+            }
+
+            @book_info_arr[3] = @book_info
+        end
+
         scraperLibraccio
         scraperLibreriaUniversitaria
+        scraperMondadori
+        scraperUnilibro
         
     end
 
--- app/scraper/scraper.rb
+++ app/scraper/scraper.rb
@@ -4,14 +4,37 @@ require 'byebug'
 
 def scraper
     #pagina target
-    url = 'https://www.amazon.it/s?k=9788808420206&crid=3DEM48XS6UPSJ&sprefix=%2Caps%2C167&ref=nb_sb_ss_recent_1_0_recent'
+    url = 'https://www.unilibro.it/libri/9788808420206'
     #html raw
     unparsed_page = HTTParty.get(url)
     #html codificato
     parsed_page = Nokogiri::HTML(unparsed_page)
 
     author_link = []
-    author = []
+        author = []
+
+        for i  in 0..parsed_page.css("li > span + a").size()-1 do
+            #raccolgo gli author link
+            author_link[i] = parsed_page.css("li > span + a")[i].attribute("href").value
+            #raccolgo gli author
+            author[i] = parsed_page.css("li > span + a")[i].text
+        end
+
+        @book_info = {
+            title: parsed_page.css("h1.title").text,
+            img: parsed_page.css("div.cover > img").attribute("src").value,
+            author: author,
+            author_link: author_link,
+            prize: parsed_page.css("li.pVendita.droid > span").text,
+            url: url,
+            site: "Unilibro"
+        },
+        img: "https://www.mondadoristore.it/"+(parsed_page.css(".span2.spanfirst .product-image-max-heigth.new-image-gallery").attribute("src").value),
+        author: author,
+        author_link: author_link,
+        prize: parsed_page.css("span.new-price").text,
+        url: url,
+        site: "Mondadori"
     
 
 
