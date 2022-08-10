3d32032c9968fe7ecd0141f72ba4ab8fc1e5dd05
Valerio Oddi <oddi.1886952@studenti.uniroma1.it>
2022-08-10 13:40:25 +0200

    aggiunto tasto preferiti

--- app/controllers/secondhands_controller.rb
+++ app/controllers/secondhands_controller.rb
@@ -85,7 +85,7 @@ class SecondhandsController < ApplicationController
     @secondhand.save
 
     respond_to do |format|
-      format.html { redirect_to secondhands_url, notice: "Secondhand was successfully approved." }
+      format.html { redirect_to library_url, notice: "Secondhand was successfully approved." }
       format.json { head :no_content }
     end
   end
