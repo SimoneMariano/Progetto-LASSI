77cf32200ade967755b7406f27ab9a30ce410243
Simone Mariano <simone.mariano1301@gmail.com>
2022-09-14 20:06:19 +0200

    Implementato calendar nella prenotazione dei posti

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,7 +5,11 @@ class ReservationsController < ApplicationController
 
   # GET /reservations or /reservations.json
   def index
-    @reservations = Reservation.all
+    if current_user.admin?
+      @reservations = Reservation.all
+    else
+      @reservations = Reservation.where(user_id: current_user.id)
+    end
   end
 
   # GET /reservations/1 or /reservations/1.json
@@ -36,13 +40,58 @@ class ReservationsController < ApplicationController
       @reservation.user_id = @user.id
     end
 
-    @reservation.startDate = Date.today
+    if current_user.provider == "google_oauth2"      
+      service = Google::Apis::CalendarV3::CalendarService.new
+      service.authorization = session[:access_token]
+      service.key = Rails.application.credentials.dig(:calendar_key)
+      event = Google::Apis::CalendarV3::Event.new(
+        summary: @reservation.seat.description,
+        location: 'Sapienza Università di Roma, Piazzale Aldo Moro, 5, 00185 Roma RM, Italia',
+        description:  @reservation.seat.name,
+        start: Google::Apis::CalendarV3::EventDateTime.new(
+          date_time: @reservation.startDate.to_datetime
+        ),
+        end: Google::Apis::CalendarV3::EventDateTime.new(
+          date_time: @reservation.endDate.to_datetime
+        ),
+        attendees: [
+          Google::Apis::CalendarV3::EventAttendee.new(
+            email: current_user.email
+          ),
+          Google::Apis::CalendarV3::EventAttendee.new(
+            email: "librarianassi@gmail.com"
+          )
+        ],
+        reminders: Google::Apis::CalendarV3::Event::Reminders.new(
+          use_default: false,
+          overrides: [
+            Google::Apis::CalendarV3::EventReminder.new(
+              reminder_method: 'email',
+              minutes: 24 * 60
+            ),
+            Google::Apis::CalendarV3::EventReminder.new(
+              reminder_method: 'popup',
+              minutes: 10
+            )
+          ]
+        )
+      )
+    end
+
+    if event_created = service.insert_event("primary", event)
+      calendar_id = event_created.id
+      @reservation.calendar_id = calendar_id
+    end
+
 
     respond_to do |format|
       if @reservation.save
         format.html { redirect_to reservation_url(@reservation), notice: "Reservation was successfully created." }
         format.json { render :show, status: :created, location: @reservation }
       else
+        if calendar_id.present? 
+          service.delete_event("primary", calendar_id)
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @reservation.errors, status: :unprocessable_entity }
       end
@@ -64,6 +113,13 @@ class ReservationsController < ApplicationController
 
   # DELETE /reservations/1 or /reservations/1.json
   def destroy
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = session[:access_token]
+    service.key = Rails.application.credentials.dig(:key)
+       
+    service.delete_event("primary", @reservation.calendar_id)
+
     @reservation.destroy
 
     respond_to do |format|
