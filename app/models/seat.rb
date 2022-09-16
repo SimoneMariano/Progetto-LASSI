2b05d96b33b33f46c1352ce71aff6b0d45c370a5
Valerio Oddi <oddi.1886952@studenti.uniroma1.it>
2022-09-16 13:12:47 +0200

    sistemati constarint db e reservation controller

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -3,4 +3,6 @@ class Seat < ApplicationRecord
     has_one :user, :through => :reservation
     has_one :reservation, dependent: :destroy
 
+    validates :name, presence: true, uniqueness: true
+
 end
